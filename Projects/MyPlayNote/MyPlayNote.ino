dae975ccf8bb35be8dc88c518c603ab1de2aad0e
jav26122 <jav26122@gmail.com>
2019-11-21 14:37:02 -0500

    .

--- Projects/MyPlayNote/MyPlayNote.ino
+++ Projects/MyPlayNote/MyPlayNote.ino
@@ -5,6 +5,7 @@
 
 
 Zumo32U4Buzzer buzzer;
+Zumo32U4ButtonB buttonB;
 
 void myPlayNote(int note, float duration, int octave)
 {
@@ -35,26 +36,43 @@ void setup() {
 
 void CMajor(int vol)
 {
-  myPlayNote(C, 500, vol);
-  myPlayNote(D, 500, vol);
-  myPlayNote(E, 500, vol);
-  myPlayNote(F, 500, vol);
-  myPlayNote(G, 500, vol);
-  myPlayNote(A, 500, vol);
-  myPlayNote(B, 500, vol);
   myPlayNote(C, 1000, vol);
-  myPlayNote(B, 500, vol);
-  myPlayNote(A, 500, vol);
-  myPlayNote(G, 500, vol);
-  myPlayNote(F, 500, vol);
-  myPlayNote(E, 500, vol);
+  delay(1000);
   myPlayNote(D, 500, vol);
+  delay(500);
+  myPlayNote(E, 500, vol);
+  delay(500);
+  myPlayNote(F, 500, vol);
+  delay(500);
+  myPlayNote(G, 500, vol);
+  delay(500);
+  myPlayNote(A, 500, vol);
+  delay(500);
+  myPlayNote(B, 500, vol);
+  delay(500);
   myPlayNote(C, 1000, vol);
-
+  delay(1000);
+  myPlayNote(B, 500, vol);
+  delay(500);
+  myPlayNote(A, 500, vol);
+  delay(500);
+  myPlayNote(G, 500, vol);
+  delay(500);
+  myPlayNote(F, 500, vol);
+  delay(500);
+  myPlayNote(E, 500, vol);
+  delay(500);
+  myPlayNote(D, 500, vol);
+  delay(500);
+  myPlayNote(C, 1000, vol);
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  myPlayNote(C, 500, 3);
-  delay(1000);
+  if (buttonB.isPressed())
+  {
+    CMajor(1);
+    delay(1000);
+  }
 }
